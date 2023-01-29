fcb7661c86e95e33272fd7ffd865575211959448
tlkbsnt <109103556+tlkbsnt@users.noreply.github.com>
2023-01-30 00:35:57 +0100

    Update assignmentFlightEasy.py

--- dags/assignmentFlightEasy.py
+++ dags/assignmentFlightEasy.py
@@ -1,24 +1,23 @@
 # Tilak Basnet  (tilak.basnet@epita.fr)
-# Note before execute the the this file install airfolw or run  docker and  docker composer file 
-
 import requests
 import json
 from airflow.decorators import dag, task
 from datetime import datetime, date
 from time import mktime
 
-
 @dag(
-  dateOfStart=datetime(2023, 1, 21),
-  scheduleOfinterval="0 1 * * *",  
+  start_date=datetime(2023, 1, 21),
+  schedule_interval="0 1 * * *",  
 )
 def assignment_of_esay_flights():
-  URL= "https://opensky-network.org/api"
+
+  BASE_URL = "https://opensky-network.org/api"
+
   def sinceEpoch(inputDate: str) -> int:
     return int(mktime(date.fromisoformat(inputDate).timetuple()))
 
   # writing to file in flightseasy.json
-  @task(multiple_outputs=True)
+  
   @task
   def dataWrite(flights: dict) -> None:
     with open("./dags/flightsEasy.json", "w") as f:
@@ -33,11 +32,11 @@ def assignment_of_esay_flights():
         "begin": sinceEpoch("2022-12-01"),
         "end": sinceEpoch("2022-12-02")
     }
-    flightOfCDG = f"{URL}/flights/departure"
+    flightOfCDG = f"{BASE_URL}/flights/departure"
     response = requests.get(flightOfCDG, params=params)
     flights = response.json()
-    print("Flights Json Data :"+ json.dumps(flights))
-    return {"flights Data: ": flights}
+    print(json.dumps(flights))
+    return {"flights": flights}
 
   flights = dataRead()
   dataWrite(flights)
